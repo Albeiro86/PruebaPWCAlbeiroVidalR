9da01ab84dba234d8c0e5e4d6709f54dcfb87d8c
Albeiro Vidal <albeiro.vidal.rojas@gmail.com>
2022-04-07 08:24:22 -0500

    feat: con carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -12,17 +15,21 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { PagesComponent } from './pages/pages.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NoFoundComponent,
     DashboardComponent,    
-    HeaderComponent, PagesComponent
+    HeaderComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,15 +2,29 @@
 ***Albeiro Vidal Rojas** */
 
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class DashboardComponent implements OnInit {
+  images = [
+    {title: 'imagePWC1', src: "./assets/images/carousel/1.png"},
+    {title: 'imagePWC2', src: "./assets/images/carousel/2.png"},
+    {title: 'imagePWC3', src: "./assets/images/carousel/3.png"},
+    {title: 'imagePWC4', src: "./assets/images/carousel/4.png"},
+    {title: 'imagePWC5', src: "./assets/images/carousel/5.png"},
+    
+  ];
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 1800;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+   }
 
   ngOnInit(): void {
   }
